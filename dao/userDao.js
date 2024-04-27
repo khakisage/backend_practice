b81efb692a2d92e86301484e686387920802f70c
khakisage <jongjin201@gmail.com>
2024-04-27 12:27:03 +0900

    userDao - selectUser 메서드 구현

--- dao/userDao.js
+++ dao/userDao.js
@@ -12,6 +12,19 @@ const userDao = {
       });
     });
   },
+  // 로그인 시 유저 정보 조회
+  selectUser(params) {
+    return new Promise((resolve, reject) => {
+      User.findOne({
+        attributes: ['id', 'userId', 'password', 'role'],
+        where: [{ userId: params.userId }],
+      }).then((selectedUser) => {
+        resolve(selectedUser);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  },
 };
 
 module.exports = userDao;
