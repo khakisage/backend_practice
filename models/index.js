815431429cf39d9d5ff385647c00e66d60d94353
khakisage <jongjin201@gmail.com>
2024-04-29 00:31:58 +0900

    관계 설정 코드 추가

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const { sequelize } = require('./connection');
 const User = require('./user');
 const Product = require('./product');
 const Category = require('./category');
+const Review = require('./review');
 
 const db = {};
 
@@ -11,10 +12,18 @@ db.sequelize = sequelize;
 db.User = User;
 db.Product = Product;
 db.Category = Category;
+db.Review = Review;
 
 //! model init(초기화)
 User.init(sequelize);
 Product.init(sequelize);
 Category.init(sequelize);
+Review.init(sequelize);
+
+//! model associate(관계 설정)
+User.associate(db);
+Product.associate(db);
+Category.associate(db);
+Review.associate(db);
 
 module.exports = db;
